b634b1579fec8aa850763e8c4d6c8970d8a6dea0
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-18 06:27:56 -0500

    create end_item_table

---
+++ R/end_item_table.R
@@ -0,0 +1,10 @@
+
+end_item_table <- tribble(~ID,          ~Name,
+                          "1_Common",	"Common",
+                          "2_CTV_GP",	"Cerberus Tactical Vehicle - General Purpose Variant",
+                          "3_CTV_C2",	"Cerberus Tactical Vehicle - Command & Control Variant",
+                          "4_CTV_HW",	"Cerberus Tactical Vehicle - Heavy Weapons Variant",
+                          "5",	        "Combat Bumper Kit",
+                          "6",	        "Fording Kit",
+                          "7",	        "Winch Kit"
+)
