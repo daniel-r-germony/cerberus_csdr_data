47c65c7e2f78c81049a2ea47961607f6362fd997
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-18 06:27:38 -0500

    create clin_table

---
+++ R/clin_table.R
@@ -0,0 +1,32 @@
+
+clin_table <- tibble(
+    ID = factor(c("0001", "0002", "0003", "0004", "0005", "0006",
+                  "1001", "1002", "1003", "1004", "1005", "1006",
+                  "2001", "2002", "2003", "2004", "2005", "2006",
+                  "3001", "3002", "3003", "3004", "3005", "3006",
+                  "4001", "4002", "4003", "4004", "4005", "4006"
+)))
+
+clin_table <- clin_table %>%
+    mutate(Name = case_when(
+        str_ends(ID, "01") ~ "General Purpose Variant",
+        str_ends(ID, "02") ~ "Command & Control Variant",
+        str_ends(ID, "03") ~ "Heavy Weapons Variant",
+        str_ends(ID, "04") ~ "Kits",
+        str_ends(ID, "05") ~ "Test & Evaluation Support",
+        str_ends(ID, "06") ~ "Systems Engineering & Systems Engineering",
+        TRUE ~ NA_character_
+        )
+    )
+
+clin_table <- clin_table %>%
+    mutate(ContractTypeID = case_when(
+        str_ends(ID, "01") ~ "FFP",
+        str_ends(ID, "02") ~ "FFP",
+        str_ends(ID, "03") ~ "FFP",
+        str_ends(ID, "04") ~ "FFP",
+        str_ends(ID, "05") ~ "CPFF",
+        str_ends(ID, "06") ~ "CPFF",
+        TRUE ~ NA_character_
+        )
+    )
