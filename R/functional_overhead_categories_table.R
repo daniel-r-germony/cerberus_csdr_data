2dee05ebaf4c45bec15f649b34cb74e81e48664a
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-18 20:33:11 -0500

    create functional_overhead_catigories_table

---
+++ R/functional_overhead_categories_table.R
@@ -0,0 +1,9 @@
+
+functional_overhead_categories_table <- oes_data %>%
+    select(occ_code, occ_group, occ_title) %>%
+    filter(occ_group == "major") %>%
+    separate(col = occ_code,
+             into = c("ID", ".drop"),
+             sep = "-") %>%
+    select(ID, Name = occ_title) %>%
+    mutate(Name = paste0(Name, " Overhead"))
