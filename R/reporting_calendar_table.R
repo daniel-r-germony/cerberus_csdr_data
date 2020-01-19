992791ab9fa93b4b4d0fa9fd7edf0d31fb520840
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-18 20:35:07 -0500

    create reporting_calendar_table

---
+++ R/reporting_calendar_table.R
@@ -0,0 +1,9 @@
+
+start_date <- "2020-01-01"
+end_date <- "2022-04-01"
+
+reporting_calendar_table <- tibble(
+    ID = 1:{seq.Date(as.Date(start_date), as.Date(end_date), by = "month") %>% length()},
+    StartDate = seq.Date(as.Date(start_date), as.Date(end_date), by = "month"),
+    EndDate = rollback(StartDate + 31)
+)
