cde21104a08cf5bd91285e38320a91c43afcbae6
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-19 06:15:12 -0500

    pull three part template comments into a df

---
+++ R/ff_comments.R
@@ -0,0 +1,32 @@
+
+library(tidyr)
+library(dplyr)
+
+extract_comment <- function(xlsx_object) {
+    xlsx_object %>%
+      select(sheet, character, comment) %>%
+      filter(!is.na(comment)) %>%
+      mutate(comment = str_remove_all(comment, pattern = "Supported values:")) %>%
+      separate_rows(comment, sep = "\r\n") %>%
+      filter(comment != "") %>%
+      rename(catigory = character) %>%
+      mutate(catigory = str_replace(catigory, pattern = "\\r\\n", replacement = " "))
+}
+
+ff_comments <- extract_comment(ff_part1) %>%
+    bind_rows(., extract_comment(ff_part2)) %>%
+    bind_rows(., extract_comment(ff_part3))
+
+ff_comments %>% View()
+
+standard_category_id <- filter(ff_comments) %>%
+    filter(catigory == "Standard Category ID") %>%
+    select(value = comment) %>%
+    distinct() %>%
+    mutate(value = factor(value))
+
+detailed_standard_category_id <- filter(ff_comments) %>%
+    filter(catigory == "Detailed Standard Category ID") %>%
+    select(value = comment) %>%
+    distinct() %>%
+    mutate(value = factor(value))
