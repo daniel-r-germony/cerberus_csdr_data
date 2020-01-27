35de7622b47ab703ff74f6bf7c9a5310ff545e36
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-27 04:58:13 -0500

    added start and end date columns

--- R/order_or_lots_table.R
+++ R/order_or_lots_table.R
@@ -25,3 +25,23 @@ order_or_lots_table <- order_or_lots_table %>%
         "PROCUREMENT",
         levels = c("RDTE", "PROCUREMENT", "O_AND_M"))
 )
+
+order_or_lots_table <- order_or_lots_table %>%
+    mutate(
+        PeriodOfPerformance_StartDate = case_when(
+            ID == "1_BA" ~ reporting_calendar_table[[1, "StartDate"]],
+            ID == "2_L1" ~ reporting_calendar_table[[7, "StartDate"]],
+            ID == "3_L2" ~ reporting_calendar_table[[13, "StartDate"]],
+            ID == "4_F1" ~ reporting_calendar_table[[19, "StartDate"]],
+            ID == "5_F2" ~ reporting_calendar_table[[24, "StartDate"]],
+            TRUE ~ as.Date(NA)
+            ),
+        PeriodOfPerformance_EndDate = case_when(
+            ID == "1_BA" ~ reporting_calendar_table[[6, "EndDate"]],
+            ID == "2_L1" ~ reporting_calendar_table[[12, "EndDate"]],
+            ID == "3_L2" ~ reporting_calendar_table[[18, "EndDate"]],
+            ID == "4_F1" ~ reporting_calendar_table[[23, "EndDate"]],
+            ID == "5_F2" ~ reporting_calendar_table[[28, "EndDate"]],
+            TRUE ~ as.Date(NA)
+            )
+        )
