1632258671f041053f05b9c51f8677e5657aeceb
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-01-31 04:28:17 -0500

    fixed a discrpency between the CSDR Plan reported as of dates and the reproting calendar

--- R/order_or_lots_table.R
+++ R/order_or_lots_table.R
@@ -30,17 +30,17 @@ order_or_lots_table <- order_or_lots_table %>%
     mutate(
         PeriodOfPerformance_StartDate = case_when(
             ID == "1_BA" ~ reporting_calendar_table[[1, "StartDate"]],
-            ID == "2_L1" ~ reporting_calendar_table[[7, "StartDate"]],
-            ID == "3_L2" ~ reporting_calendar_table[[13, "StartDate"]],
-            ID == "4_F1" ~ reporting_calendar_table[[19, "StartDate"]],
-            ID == "5_F2" ~ reporting_calendar_table[[24, "StartDate"]],
+            ID == "2_L1" ~ reporting_calendar_table[[5, "StartDate"]],
+            ID == "3_L2" ~ reporting_calendar_table[[10, "StartDate"]],
+            ID == "4_F1" ~ reporting_calendar_table[[15, "StartDate"]],
+            ID == "5_F2" ~ reporting_calendar_table[[22, "StartDate"]],
             TRUE ~ as.Date(NA)
             ),
         PeriodOfPerformance_EndDate = case_when(
-            ID == "1_BA" ~ reporting_calendar_table[[6, "EndDate"]],
-            ID == "2_L1" ~ reporting_calendar_table[[12, "EndDate"]],
-            ID == "3_L2" ~ reporting_calendar_table[[18, "EndDate"]],
-            ID == "4_F1" ~ reporting_calendar_table[[23, "EndDate"]],
+            ID == "1_BA" ~ reporting_calendar_table[[4, "EndDate"]],
+            ID == "2_L1" ~ reporting_calendar_table[[9, "EndDate"]],
+            ID == "3_L2" ~ reporting_calendar_table[[14, "EndDate"]],
+            ID == "4_F1" ~ reporting_calendar_table[[21, "EndDate"]],
             ID == "5_F2" ~ reporting_calendar_table[[28, "EndDate"]],
             TRUE ~ as.Date(NA)
             )
