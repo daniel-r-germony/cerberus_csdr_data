768aa5951ed6375afa4429cb581dd463eb8ea85d
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-02-08 06:59:51 -0500

    added comments and continued to map columns

--- R/create_ActualCostHourData.R
+++ R/create_ActualCostHourData.R
@@ -24,9 +24,13 @@ library(magrittr)
 
 combined_hours <- bind_rows(nonrecurring_hours, recurring_hours)
 
+# Add AccountID with random credit card numbers (credit card numbers look like
+# they could be something called "account" to me)
 combined_hours <- combined_hours %>%
     mutate(AccountID = ch_credit_card_number(nrow(combined_hours)))
 
+# Mutate an OrderOrLotID column into the reporting calendar (will be used in a
+# min to map OrderOrLotID into the combined hours table))
 # TODO: This needs to be refactored with a map function so it will work with an
 # unlimited number of order lots
 reporting_calendar_table_plus <- reporting_calendar_table %>%
@@ -49,10 +53,12 @@ reporting_calendar_table_plus <- reporting_calendar_table %>%
         TRUE ~ NA_character_
     ))
 
+# Mutate OrderOrLotID into the combined_hours table.
 combined_hours <- reporting_calendar_table_plus %>%
     select(ID, OrderOrLotID) %>%
     left_join(combined_hours, ., by = c("ReportingPeriodID" = "ID"))
 
+# Mutate FunctionalOverheadCategoryID into the combined_hours table.
 combined_hours <- combined_hours %>%
     mutate(FunctionalOverheadCategoryID = ID) %>%
     separate(
@@ -61,3 +67,10 @@ combined_hours <- combined_hours %>%
         into = c("FunctionalOverheadCategoryID", ".drop"),
         sep = "-") %>% select(-.drop)
 
+# Mutate CLIN_ID with SEPM and Test CLINs crosswalked (vehicle and kits will need an allocation)
+combined_hours <- combined_hours %>%
+    left_join(functional_categories_with_clin_table %>% select(-Name, -detailed_standard_category_id),
+              by = c("ID" = "ID")) %>%
+    left_join(clin_table_plus %>% select(-ContractTypeID, CLIN_ID = ID, Name),
+              by = c("OrderOrLotID" = "OrderOrLotID", "CLIN_Name" = "Name")) %>%
+    select(-CLIN_Name)
