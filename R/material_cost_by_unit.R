03c5ac5e3c87d62bcf3539fb8890433e47494a89
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-02-08 08:28:50 -0500

    created wbs distro for material costs

--- R/material_cost_by_unit.R
+++ R/material_cost_by_unit.R
@@ -83,3 +83,75 @@ material_cost <- units_or_sublots %>%
 #     mutate(test_col = case_when(
 #                ID == "5" ~ 850 * length(percent_complete),
 #                TRUE ~ NA_real_)) %>% filter(!is.na(test_col)) %>% print(n = Inf)
+
+gp_wbs_distro <- tibble::tribble(
+                                 ~ID,                                           ~Name, ~PURCHASED_PARTS, ~PURCHASED_EQUIPMENT, ~RAW_MATERIALS, ~INTERCOMPANY_WORK_ORDERS, ~DIRECT_REPORTING_SUBCONTRACTOR,
+                           "1.1.1.1", "CAV Integration, Assembly, Test, and Checkout",                0,                    0,            0.1,                         0,                               0,
+                           "1.1.1.2",                           "Hull/Frame/Body/Cab",              0.1,                    0,            0.4,                         0,                               0,
+                           "1.1.1.3",                          "System Survivability",              0.1,                 0.15,            0.1,                         0,                               0,
+                           "1.1.1.4",                               "Turret Assembly",                0,                    0,              0,                         0,                               0,
+                           "1.1.1.5",                           "Suspension/Steering",             0.25,                  0.2,           0.15,                         0,                               0,
+                           "1.1.1.6",                           "Vehicle Electronics",             0.15,                  0.1,            0.1,                         0,                               0,
+                     "1.1.1.7.1.1.1",                                  "Engine Block",              0.2,                  0.1,           0.05,                         0,                               0,
+                     "1.1.1.7.1.1.2",                                  "Turbocharger",             0.05,                    0,              0,                         0,                               0,
+                     "1.1.1.7.1.1.3",                          "Other Dressed Engine",                0,                    0,              0,                         0,                               0,
+                       "1.1.1.7.1.2",                                  "Transmission",                0,                 0.15,           0.05,                         0,                               0,
+                         "1.1.1.7.2",                                   "Drive Train",                0,                 0.05,              0,                         0,                               0,
+                           "1.1.1.8",                          "Auxiliary Automotive",                0,                    0,           0.05,                         0,                               0,
+                           "1.1.1.9",                                  "Fire Control",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.10",                                      "Armament",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.11",                 "Automatic Ammunition Handling",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.12",        "Navigation and Remote Piloting Systems",             0.15,                  0.2,              0,                         0,                               0,
+                          "1.1.1.13",                             "Special Equipment",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.14",                                "Communications",                0,                 0.05,              0,                         0,                               0,
+                          "1.1.1.15",                          "CAV Software Release",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.16",                           "Other CAV Subsystem",                0,                    0,              0,                         0,                               0
+                     )
+
+c2_wbs_distro <- tibble::tribble(
+                                 ~ID,                                           ~Name, ~PURCHASED_PARTS, ~PURCHASED_EQUIPMENT, ~RAW_MATERIALS, ~INTERCOMPANY_WORK_ORDERS, ~DIRECT_REPORTING_SUBCONTRACTOR,
+                           "1.1.1.1", "CAV Integration, Assembly, Test, and Checkout",                0,                    0,            0.1,                         0,                               0,
+                           "1.1.1.2",                           "Hull/Frame/Body/Cab",              0.1,                    0,            0.4,                         0,                               0,
+                           "1.1.1.3",                          "System Survivability",              0.1,                 0.15,            0.1,                         0,                               0,
+                           "1.1.1.4",                               "Turret Assembly",                0,                    0,              0,                         0,                               0,
+                           "1.1.1.5",                           "Suspension/Steering",             0.25,                  0.2,           0.15,                         0,                               0,
+                           "1.1.1.6",                           "Vehicle Electronics",             0.15,                  0.1,            0.1,                         0,                               0,
+                     "1.1.1.7.1.1.1",                                  "Engine Block",              0.2,                  0.1,           0.05,                         0,                               0,
+                     "1.1.1.7.1.1.2",                                  "Turbocharger",             0.05,                    0,              0,                         0,                               0,
+                     "1.1.1.7.1.1.3",                          "Other Dressed Engine",                0,                    0,              0,                         0,                               0,
+                       "1.1.1.7.1.2",                                  "Transmission",                0,                 0.15,           0.05,                         0,                               0,
+                         "1.1.1.7.2",                                   "Drive Train",                0,                 0.05,              0,                         0,                               0,
+                           "1.1.1.8",                          "Auxiliary Automotive",                0,                    0,           0.05,                       0.2,                               0,
+                           "1.1.1.9",                                  "Fire Control",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.10",                                      "Armament",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.11",                 "Automatic Ammunition Handling",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.12",        "Navigation and Remote Piloting Systems",             0.15,                  0.2,              0,                         0,                               0,
+                          "1.1.1.13",                             "Special Equipment",                0,                    0,              0,                       0.3,                               0,
+                          "1.1.1.14",                                "Communications",                0,                 0.05,              0,                       0.5,                               0,
+                          "1.1.1.15",                          "CAV Software Release",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.16",                           "Other CAV Subsystem",                0,                    0,              0,                         0,                               0
+                     )
+
+hw_wbs_distro <- tibble::tribble(
+                                 ~ID,                                           ~Name, ~PURCHASED_PARTS, ~PURCHASED_EQUIPMENT, ~RAW_MATERIALS, ~INTERCOMPANY_WORK_ORDERS, ~DIRECT_REPORTING_SUBCONTRACTOR,
+                           "1.1.1.1", "CAV Integration, Assembly, Test, and Checkout",                0,                    0,            0.1,                         0,                               0,
+                           "1.1.1.2",                           "Hull/Frame/Body/Cab",              0.1,                    0,            0.4,                         0,                               0,
+                           "1.1.1.3",                          "System Survivability",              0.1,                 0.15,            0.1,                         0,                               0,
+                           "1.1.1.4",                               "Turret Assembly",                0,                    0,              0,                         0,                               0,
+                           "1.1.1.5",                           "Suspension/Steering",             0.25,                  0.2,           0.15,                         0,                               0,
+                           "1.1.1.6",                           "Vehicle Electronics",             0.15,                  0.1,            0.1,                         0,                               0,
+                     "1.1.1.7.1.1.1",                                  "Engine Block",              0.2,                  0.1,           0.05,                         0,                               0,
+                     "1.1.1.7.1.1.2",                                  "Turbocharger",             0.05,                    0,              0,                         0,                               0,
+                     "1.1.1.7.1.1.3",                          "Other Dressed Engine",                0,                    0,              0,                         0,                               0,
+                       "1.1.1.7.1.2",                                  "Transmission",                0,                 0.15,           0.05,                         0,                               0,
+                         "1.1.1.7.2",                                   "Drive Train",                0,                 0.05,              0,                         0,                               0,
+                           "1.1.1.8",                          "Auxiliary Automotive",                0,                    0,           0.05,                         0,                               0,
+                           "1.1.1.9",                                  "Fire Control",                0,                    0,              0,                       0.4,                               0,
+                          "1.1.1.10",                                      "Armament",                0,                    0,              0,                      0.15,                               1,
+                          "1.1.1.11",                 "Automatic Ammunition Handling",                0,                    0,              0,                      0.35,                               0,
+                          "1.1.1.12",        "Navigation and Remote Piloting Systems",             0.15,                  0.2,              0,                         0,                               0,
+                          "1.1.1.13",                             "Special Equipment",                0,                    0,              0,                       0.1,                               0,
+                          "1.1.1.14",                                "Communications",                0,                 0.05,              0,                         0,                               0,
+                          "1.1.1.15",                          "CAV Software Release",                0,                    0,              0,                         0,                               0,
+                          "1.1.1.16",                           "Other CAV Subsystem",                0,                    0,              0,                         0,                               0
+                     )
